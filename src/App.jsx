252419215cc968e1283d517d2765f3a36611b53f
Abdul-0006 <154364633+Abdul-0006@users.noreply.github.com>
2024-12-08 18:59:49 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CALCULATOR from './components/Calculator';
+
+const App = () => {
+  return (
+    <div>
+  <CALCULATOR />
+    </div>
+  )
+}
+
+export default App
